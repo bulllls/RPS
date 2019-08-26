6b1fe337de16c85f3571ab7b38b441321b2bb1f6
Ruslan <ruslanfilistovich@gmail.com>
2019-08-26 14:47:22 +0300

    add func getResult()

--- RPS/Sign.swift
+++ RPS/Sign.swift
@@ -11,10 +11,6 @@ import GameplayKit
 
 let randomChoice = GKRandomDistribution(lowestValue: 0, highestValue: 2)
 
-enum Sign {
-    case rock, paper, scissors
-}
-
 func randomSign() -> Sign {
     let sign = randomChoice.nextInt()
     if sign == 0 {
@@ -23,5 +19,52 @@ func randomSign() -> Sign {
         return .paper
     } else {
         return .scissors
+    }
+}
+
+enum Sign {
+    case rock, paper, scissors
+    
+    func getResult(_ oposite: Sign) -> GameState {
+        switch self {
+        case .rock:
+            switch oposite {
+            case .rock:
+                return .draw
+            case .paper:
+                return .lose
+            case .scissors:
+                return .win
+            }
+        case .paper:
+            switch oposite {
+            case .rock:
+                return .win
+            case .paper:
+                return .draw
+            case .scissors:
+                return .lose
+            }
+        case .scissors:
+            switch oposite {
+            case .rock:
+                return .lose
+            case .paper:
+                return .win
+            case .scissors:
+                return .draw
+            }
+        }
+    }
+    
+    var emoji: String {
+        switch self {
+        case .paper:
+            return "✋🏻"
+        case .rock:
+            return "✊🏻"
+        case .scissors:
+            return "✌🏻"
+        }
     }
 }
