2c24e1bbab146c5edb050b48545cb947adf102ed
Ruslan <ruslanfilistovich@gmail.com>
2019-08-26 12:29:49 +0300

    add file GameState with enum

---
+++ RPS/GameState.swift
@@ -0,0 +1,14 @@
+//
+//  GameState.swift
+//  RPS
+//
+//  Created by Ruslan on 8/26/19.
+//  Copyright © 2019 Ruslan Filistovich. All rights reserved.
+//
+
+import Foundation
+
+enum GameState {
+    case start, win, lose, draw
+}
+
